35674e06cd393c61baea92d80357eee3fc04ff81
Rupinder <rupindermonga@gmail.com>
2020-06-20 17:16:53 +0530

    completed code, need to convert to csv

--- summary.py
+++ summary.py
@@ -1,4 +1,4 @@
-import os, re, copy
+import os, re, copy, time, pandas as pd
 
 
 path = '/media/rupinder/C49A5A1B9A5A0A76/Users/Rupinder/Desktop/BVR/Warning_code/logs'
@@ -21,7 +21,6 @@ def readingLog(file):
         if "feature_mentions" in line:
             total_features = line.count(":") - 5
             position = len(line)
-            print(position)
             feature_list = []
             for i in range(total_features):
                 position = line.rfind(':', 0, position)
@@ -31,6 +30,7 @@ def readingLog(file):
             break
     for eachFeature in feature_list:
         feature_dict[eachFeature] = {"negative": 0, "neutral": 0, "positive": 0}
+    feature_dict_copy = copy.deepcopy(feature_dict)
     for line in f:
         if "Scoring product" in line:
             new_product = re.findall(r'"([^"]*)"', line)[-1]
@@ -39,7 +39,9 @@ def readingLog(file):
                 for eachFeature in feature_list:
                     feature_position = line.find(eachFeature) + len(eachFeature) + 1
                     eachFeature_count = int(line[feature_position:feature_position+3].replace(" ","").replace(",",""))
-                    feature_dict[eachFeature]["negative"] = eachFeature_count
+                    if old_product != new_product:
+                        feature_dict[eachFeature]["negative"] = 0
+                    feature_dict[eachFeature]["negative"] += eachFeature_count
                 neg_count = line.find("negative: :") + len("negative: :")
                 if old_product != new_product:
                     negative_count = 0
@@ -48,7 +50,9 @@ def readingLog(file):
                 for eachFeature in feature_list:
                     feature_position = line.find(eachFeature) + len(eachFeature) + 1
                     eachFeature_count = int(line[feature_position:feature_position+3].replace(" ","").replace(",",""))
-                    feature_dict[eachFeature]["neutral"] = eachFeature_count
+                    if old_product != new_product:
+                        feature_dict[eachFeature]["neutral"] = 0
+                    feature_dict[eachFeature]["neutral"] += eachFeature_count
                 neu_count = line.find("neutral: :") + len("neutral: :")
                 if old_product != new_product:
                     neutral_count = 0
@@ -57,16 +61,19 @@ def readingLog(file):
                 for eachFeature in feature_list:
                     feature_position = line.find(eachFeature) + len(eachFeature) + 1
                     eachFeature_count = int(line[feature_position:feature_position+3].replace(" ","").replace(",",""))
-                    feature_dict[eachFeature]["positive"] = eachFeature_count
+                    if old_product != new_product:
+                        feature_dict[eachFeature]["positive"] = 0
+                    feature_dict[eachFeature]["positive"] += eachFeature_count
                 pos_count = line.find("positive: :") + len("positive: :")
                 if old_product != new_product:
                     positive_count = 0
                     old_product = new_product
                 positive_count += int(line[pos_count:pos_count+3].replace(" ","").replace(",",""))
-        product_details[new_product] = [{"negative": negative_count, "neutral": neutral_count, "positive": positive_count}, feature_dict]
+                feature_dict_copy = copy.deepcopy(feature_dict)
+        product_details[new_product] = [{"negative": negative_count, "neutral": neutral_count, "positive": positive_count}, feature_dict_copy]
     product_details.pop('')
     return product_details
 
-# final = readingLog('BVRScoring.2020-06-13_132713841860 (copy).log')
-final = readingLog('check.log')
+final = readingLog('BVRScoring.2020-06-13_132713841860 (copy).log')
+# final = readingLog('check.log')
 print(final)
