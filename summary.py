c690e3c31ea385cd6c8a1bb60d8706f6241cd457
Rupinder <rupindermonga@gmail.com>
2020-06-20 16:24:35 +0530

    added feature wise negative, positive, neutral. summing is left

--- summary.py
+++ summary.py
@@ -8,34 +8,62 @@ def readingLog(file):
     updated_file = os.path.join(path, file) 
     with open(updated_file) as f:
         f = f.readlines()
-
+    total_features = 0
+    feature_dict = {}
     product_details = {}
     new_product = ""
     old_product = ""
     negative_count = 0
     positive_count = 0
     neutral_count = 0
+    eachFeature_count = 0
+    for line in f:
+        if "feature_mentions" in line:
+            total_features = line.count(":") - 5
+            position = len(line)
+            print(position)
+            feature_list = []
+            for i in range(total_features):
+                position = line.rfind(':', 0, position)
+                co_position = line.rfind(",",0,position)
+                feature_list.append(line[co_position+2:position])
+                position -= 1
+            break
+    for eachFeature in feature_list:
+        feature_dict[eachFeature] = {"negative": 0, "neutral": 0, "positive": 0}
     for line in f:
         if "Scoring product" in line:
             new_product = re.findall(r'"([^"]*)"', line)[-1]
         if "feature_mentions" in line:
             if "negative: :" in line:
+                for eachFeature in feature_list:
+                    feature_position = line.find(eachFeature) + len(eachFeature) + 1
+                    eachFeature_count = int(line[feature_position:feature_position+3].replace(" ","").replace(",",""))
+                    feature_dict[eachFeature]["negative"] = eachFeature_count
                 neg_count = line.find("negative: :") + len("negative: :")
                 if old_product != new_product:
                     negative_count = 0
                 negative_count += int(line[neg_count:neg_count+3].replace(" ","").replace(",",""))    
             if "neutral: :" in line:
+                for eachFeature in feature_list:
+                    feature_position = line.find(eachFeature) + len(eachFeature) + 1
+                    eachFeature_count = int(line[feature_position:feature_position+3].replace(" ","").replace(",",""))
+                    feature_dict[eachFeature]["neutral"] = eachFeature_count
                 neu_count = line.find("neutral: :") + len("neutral: :")
                 if old_product != new_product:
                     neutral_count = 0
                 neutral_count += int(line[neu_count:neu_count+3].replace(" ","").replace(",",""))
             if "positive: :" in line:
+                for eachFeature in feature_list:
+                    feature_position = line.find(eachFeature) + len(eachFeature) + 1
+                    eachFeature_count = int(line[feature_position:feature_position+3].replace(" ","").replace(",",""))
+                    feature_dict[eachFeature]["positive"] = eachFeature_count
                 pos_count = line.find("positive: :") + len("positive: :")
                 if old_product != new_product:
                     positive_count = 0
                     old_product = new_product
                 positive_count += int(line[pos_count:pos_count+3].replace(" ","").replace(",",""))
-        product_details[new_product] = {"negative": negative_count, "neutral": neutral_count, "positive": positive_count}
+        product_details[new_product] = [{"negative": negative_count, "neutral": neutral_count, "positive": positive_count}, feature_dict]
     product_details.pop('')
     return product_details
 
